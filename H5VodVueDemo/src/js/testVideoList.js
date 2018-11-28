383a061c69d66fd8932d42d172bb9e478c1a1454
Allen Zhao <995029442@qq.com>
2018-11-28 11:34:28 +0800

    更新包, 替换vue Demo中的失效图片

--- H5VodVueDemo/src/js/testVideoList.js
+++ H5VodVueDemo/src/js/testVideoList.js
@@ -1,35 +1,35 @@
 export const videoList = [{
   VideoId: 0,
   source: 'https://vod.olympicchannel.com/NBCR_Production_-_OCS/231/1016/GEPH-ONTHERECS02E012C-_E17101101_master.m3u8',
-  CoverURL: 'https://images.olympicchannel.com/media/images/stills/content/9432/1fc8c634a136c9e1d1cfaf6e8fe45190-296x168-crop=cm.jpg',
+  CoverURL: 'http://vod-download.cn-shanghai.aliyuncs.com/video-list-img/cctv5plus.png',
   Title: 'ARGENTINA SCORE FOOTBALL PERFECTION IN ATHENS',
   Duration: '05:37',
   Description: 'The star-studded Argentina football team went on a formidable run to secure an historic gold medal at the Olympic Games Athens 2004.'
 }, {
   VideoId: 1,
   source: 'https://vod.olympicchannel.com/NBCR_Production_-_OCS/233/968/GEPH-ONTHERECS02E015C-_E17101101_master.m3u8',
-  CoverURL: 'https://images.olympicchannel.com/media/images/stills/content/9435/303c18f3184745618afa3362e8acb0cf-296x168-crop=cm.jpg',
+  CoverURL: 'http://vod-download.cn-shanghai.aliyuncs.com/video-list-img/cctv5.jpeg',
   Title: 'THE YOUNGEST OLYMPIC CHAMPIONS IN HISTORY',
   Duration: '04:11',
   Description: 'From teenage divers to Sonja Henie and the boy with no name - a closer look at the youngest champions in Olympic history.'
 }, {
   VideoId: 2,
   source: 'https://vod.olympicchannel.com/NBCR_Production_-_OCS/233/968/GEPH-ONTHERECS02E017C-_E17101101_master.m3u8',
-  CoverURL: 'https://images.olympicchannel.com/media/images/stills/content/9469/43bf99f6394e6b4a0cb8031ff05e89fe-296x168-crop=cm.jpg',
+  CoverURL: 'http://vod-download.cn-shanghai.aliyuncs.com/video-list-img/cctv11.jpeg',
   Title: 'LAVILLENIE CLAIMS POLE VAULT RECORD IN LONDON 2012',
   Duration: '05:00',
   Description: 'French pole vaulter Renaud Lavillenie loves motorsport and drives to reach new heights, as shown by his success at London 2012 Olympics.'
 }, {
   VideoId: 3,
   source: 'https://vod.olympicchannel.com/NBCR_Production_-_OCS/1003/564/GEPH-ONTHERECS02E014C-_E17101101_master.m3u8',
-  CoverURL: 'https://images.olympicchannel.com/media/images/stills/content/9476/9109d8bfe5ba07c322c7900767702ab0-296x168-crop=cm.jpg',
+  CoverURL: 'http://vod-download.cn-shanghai.aliyuncs.com/video-list-img/cctv13.jpeg',
   Title: "UNRIVALLED FISCHER'S REMARKABLE GOLD MEDAL RUN",
   Duration: '05:46',
   Description: 'A closer look at German veteran kayaker Birgit Fischer, who competed at six Olympic Games and won an incredible eight gold medals.'
 }, {
   VideoId: 4,
   source: 'https://vod.olympicchannel.com/NBCR_Production_-_OCS/71/856/GEPH-ONTHERECS02E016C-_E17101101_master.m3u8',
-  CoverURL: 'https://images.olympicchannel.com/media/images/stills/content/9478/296554fe52d9cdf50a0d53ef97e8c170-296x168-crop=cm.jpg',
+  CoverURL: 'http://vod-download.cn-shanghai.aliyuncs.com/video-list-img/cctv4.jpeg',
   Title: 'DEVERS PIPS OTTEY IN DRAMATIC 100M IN ATLANTA 1996',
   Duration: '04:19',
   Description: 'Gail Devers edges out Merlene Ottey in a thrilling photo finish Olympic 100m final at Atlanta 1996 that could barely have been any closer.'
