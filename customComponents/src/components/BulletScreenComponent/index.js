8face9ecbbf7c7f7a127709a186bb642d4a1c966
Allen Zhao <995029442@qq.com>
2019-01-11 10:32:14 +0800

    跑马灯组件防止被删除隐藏, 记忆播放首选vid进行播放时间记录

--- customComponents/src/components/BulletScreenComponent/index.js
+++ customComponents/src/components/BulletScreenComponent/index.js
@@ -25,6 +25,11 @@ export default class BulletScreenComponent {
   }
 
   ready (player, e) {
+    // 如果是自动播放在跑马灯元素上动画设置为 paused, 防止 Safari 初始值设置为 paused 的时候不会滚动
+    if (player.getOptions().autoplay === false) {
+      this.html.style.animationPlayState = 'paused'
+    }
+
     Object.keys((this.style)).forEach(key => this.html.style[key] = this.style[key])
     let bulletHeight = this.html.offsetHeight
     let playerHeight = parseInt(player.getOptions().height.replace('px', ''))
@@ -35,6 +40,32 @@ export default class BulletScreenComponent {
       this.html.addEventListener('animationiteration', () => {
         this.html.style.top = this.randomTop(maxHeight)
       })
+    }
+  }
+
+  // 阻止跑马灯组件被删除隐藏, 加上各种类名
+  timeupdate (player, timeStamp) {
+    let el = player.el()
+    let componentEl = el.querySelector('.bullet-screen')
+    if (!componentEl) {
+      el.appendChild(this.html)
+    } else {
+      if (componentEl.className !== 'bullet-screen') {
+        componentEl.className = 'bullet-screen'
+      }
+      let cssStyles = getComputedStyle(componentEl)
+      let display = cssStyles.getPropertyValue('display')
+      let opacity = cssStyles.getPropertyValue('opacity')
+      let visibility = cssStyles.getPropertyValue('visibility')
+      if (display === 'none') {
+        componentEl.style.setProperty('display', 'block')
+      }
+      if (opacity !== '1') {
+        componentEl.style.setProperty('opacity', '1')
+      }
+      if (visibility === 'hidden') {
+        componentEl.style.setProperty('visibility', 'visible')
+      }
     }
   }
 
