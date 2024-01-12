ca423bb1342ab078c4eeea7972fe7b956f1ae734
diaoshuo.ds <diaoshuo.ds@alibaba-inc.com>
2024-01-12 10:36:35 +0800

    feat: use original JS to replace JQ in playNextComponent

--- customComponents/src/components/playerNextComponent/index.js
+++ customComponents/src/components/playerNextComponent/index.js
@@ -1,14 +1,18 @@
-import playerNextHtml from './index.html';
 import './index.css';
 
-export default class playerNextComponent {
+export default class PlayerNextComponent {
     constructor(clickHandle) {
         this.clickHandle = clickHandle;
-        this.$html = $(playerNextHtml);
+        this._html = document.createElement('div');
+        this._html.setAttribute('class', 'player-olympic-player-next');
+        let innerDiv = document.createElement('div');
+        innerDiv.setAttribute('class', 'player-olympic-player-next-tip');
+        innerDiv.textContent = "Next";
+        this._html.appendChild(innerDiv);
     }
 
     createEl(el) {
-        this.$html.click(this.clickHandle);
-        $(el).find('.prism-play-btn').after(this.$html);
+        this._html.addEventListener('click', this.clickHandle);
+        el.querySelector('.prism-play-btn').insertAdjacentElement('afterend', this._html);
     }    
 }
