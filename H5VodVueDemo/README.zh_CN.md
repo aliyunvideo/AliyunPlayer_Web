b6e0d4d0e7ca2f796b48dae0a57b2dd4978d7ac3
真岛 <youcheng.yc@alibaba-inc.com>
2024-09-26 15:21:36 +0800

    chore: upgrade Vue demo with Vue3 and Vite

--- H5VodVueDemo/README.zh_CN.md
+++ H5VodVueDemo/README.zh_CN.md
@@ -2,47 +2,26 @@
 
 其他语言: [English](https://github.com/aliyunvideo/AliyunPlayer_Web/blob/master/H5VodVueDemo/README.md)
 
-基于 Vue 的播放器单页应用, 利用 web 播放器 sdk 进行视频点播，包含播放列表、字幕、多语言、自适应码率，皮肤自定义等功能 [Web播放器文档](https://help.aliyun.com/document_detail/125570.html)
-
-![移动版](https://player.alicdn.com/aliplayer/img/h5vuedemo.png) 
+基于 Vue 的播放器单页应用, 利用 web 播放器 sdk 进行视频点播，包含播放列表、记忆播放、广告等功能 [Web播放器文档](https://help.aliyun.com/document_detail/125570.html)
 
 ## 如何使用
 
-### 安装依赖项
+### 安装依赖
 
-主要依赖有 [Vue](https://vuejs.org/), [webpack 4.X](https://webpack.js.org/), [axios](https://github.com/axios/axios), [babel](https://babeljs.io/)
+主要依赖有 [Vue](https://vuejs.org/), [vite](https://vitejs.dev/)
 
 ```bash
 $ npm install
 ```
-请使用 Node.js 10安装依赖
 
-### 开发环境下
-
-macOS, Linux系统下:
+### 开发
 
 ```bash
 $ npm run dev
 ```
 
-Windows系统下:
-
-```bash
-$ npm run dev_win
-```
-
 ### 发布
-
-通过`$ npm run build`命令, 将各种依赖打包到 disk 目录, jquery 没有单独打包, 所以 index.html 中需要修改 jquery 的引用路径。
-
-macOS, Linux系统下:
 
 ```bash
 $ npm run build
-```
-
-Windows系统下:
-
-```bash
-$ npm run build_win
 ```
---
+++ H5VodVueDemo/src/components/testVideoList.ts
@@ -0,0 +1,53 @@
+import type { PlayInfo } from "../types";
+export const videoList: PlayInfo[] = [
+  {
+    VideoId: 0,
+    Source: "https://player.alicdn.com/video/aliyunmedia.mp4",
+    CoverURL:
+      "https://img.alicdn.com/imgextra/i2/O1CN01TYyG5e1kLXQkUVz0g_!!6000000004667-0-tps-1920-1080.jpg",
+    Title: "阿里云视频云介绍1",
+    Duration: "04:22",
+    Description:
+      "移动互联网时代，视频应用已经成为每家企业的标配，可对技术团队来说，选择自己开发，还是直接使用云计算平台的视频解决方案，如何确保视频交付使用后的效果，以及视频内容安全、性价比等问题，是不是正一个个的困扰着您？",
+  },
+  {
+    VideoId: 1,
+    Source: "https://player.alicdn.com/video/editor.mp4",
+    CoverURL:
+      "https://img.alicdn.com/imgextra/i2/O1CN01SBVeGt1vL2mF2VIaW_!!6000000006155-0-tps-1280-720.jpg",
+    Title: "阿里云视频云介绍2",
+    Duration: "02:39",
+    Description:
+      "在这个14亿人的国家里，每天有100亿个视频被播放，每分钟有10万条视频被上传，每秒有数十万条微博会被讨论和转发。你知道，在这一切的背后，究竟发生着什么吗？",
+  },
+  {
+    VideoId: 2,
+    Source: "https://player.alicdn.com/video/apsaravideo4k.mp4",
+    CoverURL:
+      "https://img.alicdn.com/imgextra/i1/O1CN01EikXU21SoFAcBcwd9_!!6000000002293-0-tps-4096-2160.jpg",
+    Title: "阿里云视频云介绍3",
+    Duration: "03:12",
+    Description:
+      "重大体育赛事总是吸引着全球观众的目光，而如何高效完美地将精彩赛事的视频呈现给全球观众，是一个很大的挑战。现在有了阿里云视频服务（ApsaraVideo），它能为您提供从音视频采编、上传、转码、存储、媒资管理、分发、到播放的一站式服务。",
+  },
+  {
+    VideoId: 3,
+    Source: "https://player.alicdn.com/video/aliyunmedia.mp4",
+    CoverURL:
+      "https://img.alicdn.com/imgextra/i2/O1CN01TYyG5e1kLXQkUVz0g_!!6000000004667-0-tps-1920-1080.jpg",
+    Title: "阿里云视频云介绍4",
+    Duration: "04:22",
+    Description:
+      "移动互联网时代，视频应用已经成为每家企业的标配，可对技术团队来说，选择自己开发，还是直接使用云计算平台的视频解决方案，如何确保视频交付使用后的效果，以及视频内容安全、性价比等问题，是不是正一个个的困扰着您？",
+  },
+  {
+    VideoId: 4,
+    Source: "https://player.alicdn.com/video/editor.mp4",
+    CoverURL:
+      "https://img.alicdn.com/imgextra/i2/O1CN01SBVeGt1vL2mF2VIaW_!!6000000006155-0-tps-1280-720.jpg",
+    Title: "阿里云视频云介绍5",
+    Duration: "02:39",
+    Description:
+      "在这个14亿人的国家里，每天有100亿个视频被播放，每分钟有10万条视频被上传，每秒有数十万条微博会被讨论和转发。你知道，在这一切的背后，究竟发生着什么吗？",
+  },
+];
